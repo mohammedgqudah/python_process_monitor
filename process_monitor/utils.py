56480c82f1e63d1406ba5243e99b66936100a5a2
mqudah0031 <71003640+mqudah0031@users.noreply.github.com>
2021-09-18 16:43:36 +0300

    show days hours minutes & seconds in uptime

--- process_monitor/utils.py
+++ process_monitor/utils.py
@@ -1,7 +1,18 @@
 from datetime import timedelta
 
 
+def date_diff_in_seconds(dt2, dt1):
+    timedelta = dt2 - dt1
+    return timedelta.days * 24 * 3600 + timedelta.seconds
+
+
+def dhms_from_seconds(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    return days, hours, minutes, seconds
+
+
 def get_elapsed_time(start, end):
     """Get elapsed time in a human readable format."""
-    td: timedelta = start - end
-    return str(timedelta(seconds=td.seconds))
+    return "%s days, %s hours, %s minutes, %s seconds" % dhms_from_seconds(date_diff_in_seconds(start, end))
